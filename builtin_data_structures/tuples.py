0b7739c613df7a9d578b17bca8243b53bc1c365d
Rv607 <19752692+Rv607x@users.noreply.github.com>
2024-01-31 23:32:00 +0300

    creating a tuple, merging and indexing

---
+++ builtin_data_structures/tuples.py
@@ -0,0 +1,19 @@
+# Simmilar to lists except the contents are immuatable or cannot be changed
+
+# creating a tuple
+car = ("Ford", "Raptor", 2019, "Red")
+hero1 = ("Batman", "Bruce Wayne")
+hero2 = ("Wonder Woman", "Diana Prince")
+
+print(hero1)
+#lenght of tuple
+print(len(hero1))
+
+#indexing a tuple
+print(car[1])
+
+#slicing a tuple 
+print(car[2:])
+
+# Merging Tuples
+       
