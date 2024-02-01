d3539263ff9dc49cfb0e7b2414412d650fe367c5
Rv607x <19752692+Rv607x@users.noreply.github.com>
2024-02-01 17:50:03 +0300

    set constructor method

--- builtin_data_structures/sets.py
+++ builtin_data_structures/sets.py
@@ -2,3 +2,12 @@
 random_set = {"Cow", 2345, 56.76, (True, False)}
 print(random_set)
 print(len(random_set))
+
+# Set constructor 
+# set() is another way of creating a set
+
+empty_set = set()
+print(empty_set)
+
+random_set2 = set({"Cow", 2345, 56.76, (True, False)})
+print(random_set2)
