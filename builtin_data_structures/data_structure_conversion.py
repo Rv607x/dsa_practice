c2ce2fe76b8ea65fc4a2895a6f3377c1a1b4f92c
Rv607x <19752692+Rv607x@users.noreply.github.com>
2024-02-01 18:24:40 +0300

    various conversions

---
+++ builtin_data_structures/data_structure_conversion.py
@@ -0,0 +1,55 @@
+# Converting to lists 
+# can convert a tuple, set, or dictionary to a list using the list() constructor. In the case of a dictionary, only the keys will be converted to a list.
+
+star_wars_tup = ("Anakin", "Darth Vader", 1000)
+print(star_wars_tup)
+star_wars_set = {"Anakin", "Darth Vader", 1000}
+print(star_wars_set)
+star_wars_dict = {1: "Anakin", 2: "Darth Vader", 3: 1000}
+print(star_wars_dict)
+
+star_wars_list = list(star_wars_tup)  # Converting from tuple
+print(star_wars_list)
+
+star_wars_list = list(star_wars_set)  # Converting from set
+print(star_wars_list)
+
+star_wars_list = list(star_wars_dict)  # Converting from dictionary
+print(star_wars_list)
+
+# Convertin to turple
+# Any data structure can be converted to a tuple using the tuple() constructor. In the case of a dictionary, only the keys will be converted to a tuple:
+star_wars_list = ["Anakin", "Darth Vader", 1000]
+print(star_wars_list)
+star_wars_set = {"Anakin", "Darth Vader", 1000}
+print(star_wars_set)
+star_wars_dict = {1: "Anakin", 2: "Darth Vader", 3: 1000}
+print(star_wars_dict)
+
+star_wars_tup = tuple(star_wars_list)  # Converting from list
+print(star_wars_tup)
+
+star_wars_tup = tuple(star_wars_set)  # Converting from set
+print(star_wars_tup)
+
+star_wars_tup = tuple(star_wars_dict)  # Converting from dictionary
+print(star_wars_tup)
+
+
+#converting to set
+# The set() constructor can be used to create a set out of any other data structure. In the case of a dictionary, only the keys will be converted to a set:
+star_wars_list = ["Anakin", "Darth Vader", 1000]
+print(star_wars_list)
+star_wars_tup = ("Anakin", "Darth Vader", 1000)
+print(star_wars_tup)
+star_wars_dict = {1: "Anakin", 2: "Darth Vader", 3: 1000}
+print(star_wars_dict)
+
+star_wars_set = set(star_wars_list)  # Converting from list
+print(star_wars_set)
+
+star_wars_set = set(star_wars_tup)  # Converting from tuple
+print(star_wars_set)
+
+star_wars_set = set(star_wars_dict)  # Converting from dictionary
+print(star_wars_set)
