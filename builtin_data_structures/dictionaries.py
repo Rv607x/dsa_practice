6ee46a33cc8fc42dc5f7fa382cc1c33ab3e302bf
Rv607x <19752692+Rv607x@users.noreply.github.com>
2024-02-01 16:46:02 +0300

    added dictionary creation method

---
+++ builtin_data_structures/dictionaries.py
@@ -0,0 +1,11 @@
+# Creating a dictionary
+empty_dict = {}
+print(empty_dict)
+
+phone_book = {"Batman": 42786,
+              "Cersei": 76345,
+              "Ghostbusters": 63778
+              }
+print(phone_book)
+
+# dict() constructore
