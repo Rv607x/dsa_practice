b8cc6b144b981185c4f03ce5bc65b7bfc5eda05f
Rv607x <19752692+Rv607x@users.noreply.github.com>
2024-02-01 20:11:37 +0300

    added peek, is empty, get stack and pop methods to stack

--- stack/stack.py
+++ stack/stack.py
@@ -14,7 +14,38 @@ class Stack():
         self.items.append(item)
 
     def pop(self):
+        """
+        Remove the top element of the stack
+        """
         return self.items.pop()
-    
 
+    def is_empty(self):
+        """
+        check if the stack is empty
+        """
+        return self.items == []
+
+    def peek(self):
+        """
+        Used to view the top element of the stack 
+        """
+        if not self.is_empty():
+            return self.items[-1]
+
+    def get_stack(self):
+        return self.items
+    
+    
 # Test functionality
+mystack = Stack()
+mystack.push("A")
+mystack.push("cow")
+print(mystack.get_stack())
+mystack.push("C")
+print(mystack.get_stack())
+mystack.pop()
+print(mystack.get_stack())
+print(mystack.peek())
+
+newStack = Stack()
+print(newStack.is_empty())
