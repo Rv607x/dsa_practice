9fb1aae6e6b1d28685da51db2eaf1a70d7cb149a
Rv607x <19752692+Rv607x@users.noreply.github.com>
2024-02-01 16:53:10 +0300

    ädded dictionary constructor method

--- builtin_data_structures/dictionaries.py
+++ builtin_data_structures/dictionaries.py
@@ -8,4 +8,18 @@ phone_book = {"Batman": 42786,
               }
 print(phone_book)
 
-# dict() constructore
+# dict() constructor
+# used to construct a dictionary
+
+empty_dict2 = dict() #create empty dictionary
+print(empty_dict2)
+
+phone_book2 = dict(Batman=42786, Cersei = 76345, Ghostbusters= 63778)
+print(phone_book2)
+
+# Alternative approach 
+phone_book3 = dict([('Batman', 468426),
+                   ('Cersei', 237734),
+                   ('Ghostbusters', 44678)])
+print(phone_book3)
+
