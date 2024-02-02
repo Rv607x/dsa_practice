130eeee338d5fcd9abb224e8244c6628f4f77365
Rv607x <19752692+Rv607x@users.noreply.github.com>
2024-02-02 10:47:15 +0300

    used python inbuilt lists to reverse a string

---
+++ stack/reverse_string_lst.py
@@ -0,0 +1,15 @@
+def reverse_string(input_str: str):
+    empty_lst = []
+    new_str = ""
+    if len(input_str) == 0:
+        return input_str
+    else:
+        for i in input_str:
+            empty_lst.append(i)
+    while len(empty_lst) > 0:      
+        new_str += empty_lst.pop()
+    return new_str
+
+input_str = "!evitacudE ot emocleW"
+print(reverse_string(input_str))
+print(reverse_string(""))
