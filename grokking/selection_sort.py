b3c5b0263c9bfe3ac712d628b5702319bcec6c81
Rv607 <19752692+Rv607x@users.noreply.github.com>
2024-02-03 10:49:56 +0300

    added selection sort algorithm implementation

---
+++ grokking/selection_sort.py
@@ -0,0 +1,19 @@
+def find_smallest(arr):
+    """Find index of smallest item in the array"""
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+#print(find_smallest([5, 3, 6, 2, 10]))
+
+def selection_sort(arr):
+    new_arr = []
+    copied_arr = list(arr)
+    for i in range(len(copied_arr)):
+        smallest = find_smallest(copied_arr)
+        new_arr.append(copied_arr.pop(smallest))
+    return new_arr
+print(selection_sort([5, 3, 6, 2, 10, -6]))
